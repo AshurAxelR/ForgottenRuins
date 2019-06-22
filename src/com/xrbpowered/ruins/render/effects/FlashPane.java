4fbc453e824506924515d5332982c849e836c7e9
AshurAxelR <7903175+AshurAxelR@users.noreply.github.com>
2019-06-22 12:05:49 +0100

    Player health, hud, black out, death

--- src/com/xrbpowered/ruins/render/effects/FlashPane.java
+++ src/com/xrbpowered/ruins/render/effects/FlashPane.java
@@ -15,9 +15,11 @@ public class FlashPane extends UINode {
 	private static class FlashShader extends Shader {
 		public Vector4f color = new Vector4f();
 		public float alpha = 0f;
+		public float black = 0f;
 		
 		private int colorLocation;
 		private int alphaLocation;
+		private int blackLocation;
 		
 		public FlashShader() {
 			super(PaneShader.vertexInfo, "scrn_v.glsl", "flash_f.glsl");
@@ -27,6 +29,7 @@ public class FlashPane extends UINode {
 		protected void storeUniformLocations() {
 			colorLocation = GL20.glGetUniformLocation(pId, "color");
 			alphaLocation = GL20.glGetUniformLocation(pId, "alpha");
+			blackLocation = GL20.glGetUniformLocation(pId, "black");
 		}
 		
 		@Override
@@ -43,6 +46,7 @@ public class FlashPane extends UINode {
 		public void updateUniforms() {
 			uniform(colorLocation, color);
 			GL20.glUniform1f(alphaLocation, alpha);
+			GL20.glUniform1f(blackLocation, black);
 		}
 		
 		@Override
@@ -55,8 +59,11 @@ public class FlashPane extends UINode {
 	}
 	
 	private static FlashShader shader;
-	private float startAlpha = 0f;
 	private float t = 0f;
+	private float baseAlpha = 0f;
+	private float startAlpha = 0f;
+	private float flash = 0f;
+	private boolean blackOut = false;
 	
 	public FlashPane(UIContainer parent) {
 		super(parent);
@@ -68,22 +75,51 @@ public class FlashPane extends UINode {
 		super.layout();
 	}
 	
+	public void setBaseAlpha(float health) {
+		baseAlpha = (health<25f) ? (25f-health)/100f : 0f;
+	}
+	
 	public void flashPain(float alpha) {
 		startAlpha = shader.alpha + alpha;
+		flash = 0f;
+	}
+	
+	public void flashPain(float damage, float healthBefore) {
+		flashPain(damage/healthBefore + 0.02f);
+	}
+	
+	public void blackOut() {
+		blackOut = true;
+	}
+	
+	public void reset() {
 		t = 0f;
+		baseAlpha = 0f;
+		startAlpha = 0f;
+		blackOut = false;
+		if(shader!=null) {
+			shader.alpha = 0f;
+			shader.black = 0f;
+		}
 	}
 	
 	@Override
 	public void updateTime(float dt) {
 		t += dt;
-		float a = (float)Math.pow(0.75f, t*5f) * startAlpha;
+		flash += dt;
+		float a = (float)Math.pow(0.75f, flash*5f) * startAlpha + baseAlpha * ((float)Math.sin(t*2f)*0.25f+0.75f);
 		if(a<0.01f) a = 0;
 		shader.alpha = a;
+		if(blackOut) {
+			shader.black += dt*0.5f;
+			if(shader.black>1f)
+				shader.black = 1f;
+		}
 		super.updateTime(dt);
 	}
 	
 	public void render(RenderTarget target) {
-		if(shader.alpha>0f) {
+		if(shader.alpha>0f || shader.black>0f) {
 			shader.use();
 			PaneShader.getInstance().quad.draw();
 			shader.unuse();
