07cc32c8f01e42bcfcb385a9a9248543b9294793
ashurrafiev <ashur.rafiev@gmail.com>
2019-07-03 20:17:35 +0100

    Daze effect, drowning

--- src/assets/flash_f.glsl
+++ src/assets/flash_f.glsl
@@ -7,6 +7,6 @@ uniform float black = 0;
 out vec4 out_Color;
 
 void main(void) {
-	out_Color = mix(vec4(1), color, clamp(alpha, 0, 1)*color.a) * (1-black);
+	out_Color = mix(vec4(1), color, clamp(alpha, 0, 1)*color.a) * clamp(1-black, 0, 1);
 	out_Color.a = 1;
 }
