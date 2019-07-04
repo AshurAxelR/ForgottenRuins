a7792503b73cabcc98a498eb03cf0aeb96c71981
ashurrafiev <ashur.rafiev@gmail.com>
2019-07-04 18:37:21 +0100

    JAR manifest and readme

--- src/com/xrbpowered/ruins/render/shader/ShaderEnvironment.java
+++ src/com/xrbpowered/ruins/render/shader/ShaderEnvironment.java
@@ -9,11 +9,12 @@ public class ShaderEnvironment {
 
 	public float fogNear = 10f;
 	public float fogFar = 50f;
-	public Vector4f fogColor = new Vector4f(0.4f, 0.6f, 0.9f, 1);
 
 	public Vector3f lightDirection = new Vector3f(0.15f, -0.5f, 0.3f).normalize();
 	public float ambient = 0.7f;
 	public float lightScale = 0.1f;
+
+	public Vector4f fogColor = new Vector4f(0.4f, 0.6f, 0.9f, 1);
 
 	public void setFog(float near, float far, Vector4f color) {
 		this.fogNear = near;
