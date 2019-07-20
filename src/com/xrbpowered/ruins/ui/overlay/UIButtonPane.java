7e12e5e7a0a130869d3efe2e142cccf00f899177
ashurrafiev <ashur.rafiev@gmail.com>
2019-07-20 18:48:35 +0100

    List of complete tablets

--- src/com/xrbpowered/ruins/ui/overlay/UIButtonPane.java
+++ src/com/xrbpowered/ruins/ui/overlay/UIButtonPane.java
@@ -10,12 +10,19 @@ import com.xrbpowered.zoomui.UIElement;
 
 public class UIButtonPane extends UIPane {
 
+	public static final Color disabledColor = new Color(0x555555);
+	public static final Color downColor = new Color(0xaaaaaa);
+	public static final Color hoverColor = new Color(0xdddddd);
+	public static final Color buttonColor = new Color(0xbbbbbb);
+	
 	public static final int width = 180;
 	public static final int height = 40;
 	
 	public String label;
 	public boolean down = false;
 	public boolean hover = false;
+	
+	protected boolean enabled = true;
 	
 	public UIButtonPane(UIContainer parent, String label) {
 		super(parent, true);
@@ -25,11 +32,22 @@ public class UIButtonPane extends UIPane {
 	
 	@Override
 	protected void paintSelf(GraphAssist g) {
-		g.setColor(down ? new Color(0xaaaaaa) : hover ?  new Color(0xdddddd) : new Color(0xbbbbbb));
+		g.setColor(!enabled ? disabledColor : down ? downColor : hover ?  hoverColor : buttonColor);
 		g.fill(this);
 		g.setColor(Color.BLACK);
 		g.setFont(UIHud.fontBold);
 		g.drawString(label, getWidth()/2, getHeight()/2, GraphAssist.CENTER, GraphAssist.CENTER);
+	}
+	
+	public void setEnabled(boolean enabled) {
+		if(this.enabled!=enabled) {
+			this.enabled = enabled;
+			repaint();
+		}
+	}
+	
+	public boolean isEnabled() {
+		return enabled;
 	}
 	
 	@Override
@@ -48,8 +66,10 @@ public class UIButtonPane extends UIPane {
 	@Override
 	public boolean onMouseDown(float x, float y, Button button, int mods) {
 		if(button==Button.left) {
-			down = true;
-			repaint();
+			if(enabled) {
+				down = true;
+				repaint();
+			}
 			return true;
 		}
 		return false;
@@ -58,9 +78,11 @@ public class UIButtonPane extends UIPane {
 	@Override
 	public boolean onMouseUp(float x, float y, Button button, int mods, UIElement initiator) {
 		if(button==Button.left) {
-			down = false;
-			onAction();
-			repaint();
+			if(enabled) {
+				down = false;
+				onAction();
+				repaint();
+			}
 			return true;
 		}
 		return false;
--- src/com/xrbpowered/ruins/ui/overlay/UIOverlayVerse.java
+++ src/com/xrbpowered/ruins/ui/overlay/UIOverlayVerse.java
@@ -1,5 +1,6 @@
 package com.xrbpowered.ruins.ui.overlay;
 
+import com.xrbpowered.gl.ui.UINode;
 import com.xrbpowered.gl.ui.pane.UIPane;
 import com.xrbpowered.ruins.Ruins;
 import com.xrbpowered.ruins.world.VerseSystem;
@@ -14,6 +15,8 @@ public class UIOverlayVerse extends UIOverlay {
 	public final UIText text;
 	public final UIPane info;
 	public final UIText infoText;
+	public final UINode list;
+	public final UIButtonPane[] listButtons;
 	
 	public UIOverlayVerse(UIContainer parent) {
 		super(parent);
@@ -33,6 +36,23 @@ public class UIOverlayVerse extends UIOverlay {
 		info = new UISolidPane.Clear(this);
 		info.setSize(text.getWidth()-100, 100);
 		infoText = new UIText.Small(info);
+		
+		list = new UINode(this);
+		listButtons = new UIButtonPane[VerseSystem.verses.length];
+		for(int i=0; i<VerseSystem.verses.length; i++) {
+			final int verse = i;
+			UIButtonPane btn = new UIButtonPane(list, RomanNumerals.toRoman(i+1)) {
+				@Override
+				public void onAction() {
+					show(verse);
+				}
+			};
+			btn.enabled = false;
+			btn.setSize(48, btn.getHeight());
+			btn.setLocation(i*52, 0);
+			listButtons[i] = btn;
+		}
+		list.setSize(listButtons.length*52-4, listButtons[0].getHeight());
 	}
 	
 	@Override
@@ -40,20 +60,27 @@ public class UIOverlayVerse extends UIOverlay {
 		closeButton.setLocation(getWidth()/2f-closeButton.getWidth()/2f, getHeight()-100);
 		box.setLocation(getWidth()/2f-box.getWidth()/2f, getHeight()/2f-box.getHeight()/2f);
 		info.setLocation(getWidth()/2f-info.getWidth()/2f, getHeight()-200);
+		list.setLocation(getWidth()/2f-list.getWidth()/2f, box.getY()-list.getHeight()-30);
 		super.layout();
 	}
-	
+
+	public void show(int verse) {
+		String html = String.format("<p>- %s -</p><p>%s</p>", RomanNumerals.toRoman(verse+1), VerseSystem.verses[verse].toUpperCase());
+		text.setHtml(html);
+		infoText.setHtml("");
+		Ruins.ruins.setOverlay(Ruins.overlayVerse);
+	}
+
 	public void updateAndShow(VerseSystem verses, Tablet tablet) {
 		String html = verses.access(tablet.seed, !tablet.visited);
 		text.setHtml(html);
-		text.repaint();
 		if(tablet.visited)
 			infoText.setHtml("<p>You studied this tablet before.</p>");
 		else if(verses.learned!=null)
 			infoText.setHtml("<p>You thoroughly studied the tablet and learned the word for <span class=\"e\">" + verses.learned + "</span>.</p>");
 		else
 			infoText.setHtml("<p>You read the tablet but didn't learn any new words.</p>");
-		infoText.repaint();
+		listButtons[verses.verse].setEnabled(verses.complete);
 		Ruins.ruins.setOverlay(Ruins.overlayVerse);
 	}
 
