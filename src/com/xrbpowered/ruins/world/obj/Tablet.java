64b98f0967fa7e829d1dc658a09e1e0cd1638f2b
ashurrafiev <ashur.rafiev@gmail.com>
2020-01-31 12:34:15 +0000

    Tablet glow removed

--- src/com/xrbpowered/ruins/world/obj/Tablet.java
+++ src/com/xrbpowered/ruins/world/obj/Tablet.java
@@ -7,18 +7,20 @@ import java.io.IOException;
 import com.xrbpowered.ruins.Ruins;
 import com.xrbpowered.ruins.render.prefab.Prefab;
 import com.xrbpowered.ruins.render.prefab.PrefabRenderer;
+import com.xrbpowered.ruins.ui.overlay.RomanNumerals;
 import com.xrbpowered.ruins.world.VerseSystem;
 import com.xrbpowered.ruins.world.World;
 import com.xrbpowered.ruins.world.gen.WorldGenerator.Token;
 
 public class Tablet extends TileObject {
 
+	public final int verse;
+	
 	public boolean visited = false;
-	public int verseNumber;
 	
 	public Tablet(World world, Token objToken) {
 		super(world, objToken);
-		verseNumber = VerseSystem.getVerseNumber(seed);
+		verse = VerseSystem.getVerseNumber(seed);
 	}
 
 	@Override
@@ -33,17 +35,21 @@ public class Tablet extends TileObject {
 	
 	@Override
 	public Prefab getPrefab() {
-		return world.player.verses.completeVerses[verseNumber] ? PrefabRenderer.tabletGlow : PrefabRenderer.tablet;
+		return PrefabRenderer.tablet;
+	}
+	
+	public boolean isComplete() {
+		return world.player.verses.completeVerses[verse];
 	}
 	
 	@Override
 	public String getPickName() {
-		return "Tablet";
+		return visited || isComplete() ? String.format("Tablet %s", RomanNumerals.toRoman(verse+1)) : "Tablet";
 	}
 	
 	@Override
 	public String getActionString() {
-		return visited ? "[Visited]" : "[Right-click to read]";
+		return isComplete() ? "[Complete]" :  visited ? "[Visited]" : "[Right-click to read]";
 	}
 	
 	@Override
